0b441070434ad6ab1041119a330c0fc3bcf4186f
Marco Faletra <marco.faletra@inpeco.com>
2025-11-17 23:24:08 +0100

    More Worker Effort lines

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use chrono::{Datelike, Local};
-use slint::SharedString;
+use slint::{SharedString, VecModel};
 
 slint::include_modules!();
 
-fn dates(start_date: &mut chrono::DateTime<Local>) -> Vec<SharedString> {
+fn dates(start_date: &chrono::DateTime<Local>) -> Vec<SharedString> {
     let mut dates: Vec<SharedString> = Vec::new();
-    *start_date = primo_giorno_settimana_corrente(start_date);
+    let mut start_date = primo_giorno_settimana_corrente(start_date);
+
     let week_number = 52 - start_date.iso_week().week();
     for _ in 0..52 + week_number {
         let date_str = start_date.format("%y-%m-%d").to_string();
         dates.push(date_str.clone().into());
-        *start_date += chrono::Duration::days(7);
+        start_date += chrono::Duration::days(7);
+        start_date = primo_giorno_settimana_corrente(&start_date);
     }
     dates
 }
@@ -20,38 +22,10 @@ fn primo_giorno_settimana_corrente(data: &chrono::DateTime<Local>) -> chrono::Da
     *data - chrono::Duration::days(giorni_da_lunedi as i64)
 }
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen(start))]
-fn main() {
-    use slint::Model;
-
-    let main_window = MainWindow::new().unwrap();
-
-    let mut start_date = Local::now() - chrono::Duration::days(30);
-    let model = std::rc::Rc::new(slint::VecModel::from(dates(&mut start_date)));
-
-    let worker_efforts: Vec<SharedString> = vec![
-        "Worker12345|10",
-        "Worker12|20",
-        "Worker1|10\nWorker9|10",
-        "Worker54321|10",
-        "",
-        "Worker5|25",
-        "Worker|15",
-        "Worker3|15",
-        "Worker4|65",
-        "Worker6|88",
-        "Worker789|92",
-        "Worker8|78",
-        "Worker9|82",
-    ]
-    .iter()
-    .map(|s| SharedString::from(*s))
-    .collect();
-    let worker_efforts_model = std::rc::Rc::new(slint::VecModel::from(worker_efforts));
-
+fn get_partial_efforts_model(worker_efforts: &Vec<&str>) -> std::rc::Rc<VecModel<i32>> {
     let mut partial_efforts = vec![];
     let mut effort = 0;
-    for worker_effort in worker_efforts_model.iter() {
+    for worker_effort in worker_efforts.iter() {
         let parts: Vec<&str> = worker_effort.split('\n').collect();
         if parts.is_empty() {
             partial_efforts.push(effort);
@@ -68,7 +42,55 @@ fn main() {
         partial_efforts.push(effort);
     }
 
-    let partial_efforts_model = std::rc::Rc::new(slint::VecModel::from(partial_efforts));
+    std::rc::Rc::new(slint::VecModel::from(partial_efforts))
+}
+
+fn get_worker_efforts_model(worker_efforts: &Vec<&str>) -> std::rc::Rc<VecModel<SharedString>> {
+    let shared_string: Vec<SharedString> = worker_efforts
+    .iter()
+    .map(|s| SharedString::from(*s))
+    .collect();
+    std::rc::Rc::new(slint::VecModel::from(shared_string))
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen(start))]
+fn main() {
+    let main_window = MainWindow::new().unwrap();
+
+    let start_date = Local::now() - chrono::Duration::days(30);
+    let model = std::rc::Rc::new(slint::VecModel::from(dates(&start_date)));
+
+    let worker_efforts_1 = vec![
+        "Worker12345|10",
+        "Worker12|20",
+        "Worker1|10\nWorker9|10",
+        "Worker54321|10",
+        "",
+        "Worker5|25",
+        "Worker|15",
+        "Worker3|15",
+        "Worker4|65",
+        "Worker6|88",
+        "Worker789|92",
+        "Worker8|78",
+        "Worker9|82",
+    ];
+
+    let worker_efforts_2 = vec![
+        "Worker12345|80",
+        "Worker12|80",
+        "Worker54321|80",
+        "Worker1|80\nWorker9|80\nWorker1|90",
+        "Worker5|90",
+        "Worker5|60",
+        "Worker|25",
+        "Worker3|45",
+        "Worker4|65",
+        "Worker6|88",
+        "Worker789|92",
+        "Worker8|78",
+        "Worker9|82",
+    ];
 
     let pjm_datas = PjmDatas {
         current_dates: model.clone().into(),
@@ -77,15 +99,23 @@ fn main() {
                 .format("%y-%m-%d")
                 .to_string(),
         ),
-        dev_efforts: std::rc::Rc::new(slint::VecModel::from(vec![DevEffort {
-            worker_efforts: worker_efforts_model.clone().into(),
-            partial_efforts: partial_efforts_model.clone().into(),
-            effort: 250,
-            background_color: slint::Color::from_rgb_u8(0xcc, 0xdd, 0xcc),
-        }]))
+        dev_efforts: std::rc::Rc::new(slint::VecModel::from(vec![
+            DevEffort {
+                worker_efforts: get_worker_efforts_model(&worker_efforts_1).clone().into(),
+                partial_efforts: get_partial_efforts_model(&worker_efforts_1).clone().into(),
+                effort: 250,
+                background_color: slint::Color::from_rgb_u8(0xcc, 0xdd, 0xcc),
+            },
+            DevEffort {
+                worker_efforts: get_worker_efforts_model(&worker_efforts_2).clone().into(),
+                partial_efforts: get_partial_efforts_model(&worker_efforts_2).clone().into(),
+                effort: 1000,
+                background_color: slint::Color::from_rgb_u8(0xdd, 0xcc, 0xdd),
+            }
+        ]))
         .into(),
     };
-    main_window.set_pjm_datas(pjm_datas);
+    main_window.set_pjm_datas(pjm_datas);    
 
     main_window.run().unwrap();
 }
